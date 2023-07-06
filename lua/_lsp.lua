3e4d828b4cdfacb43c50305ef9c1e2498088697b
Wynn Wilkes <wynn@l2l.com>
2023-07-06 11:19:32 -0600

    Fix a deprecation warning for the lsp capabilities api

--- lua/_lsp.lua
+++ lua/_lsp.lua
@@ -98,7 +98,7 @@ local handlers = {
 
 -- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
+capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
 
 -- Provide settings first!
 lsp_installer.settings({
